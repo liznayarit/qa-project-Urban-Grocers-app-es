18d4695ed5f040a78cbdf192eba07141adaf8d59
naya <liznayaritpabon@gmail.com>
2024-11-18 11:41:00 -0500

    qa-project-Urban-Grocers-app-es

--- configuration.py
+++ configuration.py
@@ -1,3 +1,4 @@
 URL_SERVICE = # Inserta tu dirección de URL sin la barra diagonal al final
-CREATE_USER_PATH = # Almacena la ruta para crear un usuario o usuaria en esta variable
-KITS_PATH = # Almacena la ruta para crear un kit en esta variable
+CREATE_USER_PATH = "/api/v1/users/"# Almacena la ruta para crear un usuario o usuaria en esta variable
+KITS_PATH = "/api/v1/kits/"# Almacena la ruta para crear un kit en esta variable
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
---
+++ sender_stand_request.py
@@ -0,0 +1,32 @@
+import configuration
+import requests
+import data
+
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+
+response = get_users_table()
+print(response.status_code)
+
+
+
+def post_new_client_kit(kit_body, auth_token):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,  # inserta la dirección URL completa
+                         json=kit_body,  # inserta el cuerpo de solicitud
+                         headers=data.headers)  # inserta los encabezados
+
+response = post_new_client_kit(data.user_body)
+print(response.status_code)
+print(response.json())
+
+
+def post_new_kit(body):
+    return requests.post(configuration.URL_SERVICE + configuration.KITS_PATH,  # inserta la dirección URL completa
+                         json=body,  # inserta el cuerpo de solicitud
+                         headers=data.headers)  # inserta los encabezados
+
+response = post_new_kit(data.user_body)
+print(response.status_code)
+print(response.json())
+
+
