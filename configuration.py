ad93bc53392a843bf935d1d56f89bbd6c2746d64
naya <liznayaritpabon@gmail.com>
2024-11-23 22:53:07 -0500

    qa-project-Urban-Grocers-app-es

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-3dcd879e-345c-4036-a336-6a34793cc00f.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-0095914d-64a0-4035-a9da-344108a6bbfa.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users/"# Almacena la ruta para crear un usuario o usuaria en esta variable
 KITS_PATH = "/api/v1/kits/"# Almacena la ruta para crear un kit en esta variable
--- sender_stand_request.py
+++ sender_stand_request.py
@@ -3,15 +3,10 @@ import requests
 import data
 
 
-
 def post_new_user(body):
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,  # inserta la dirección URL completa
                          json=body,  # inserta el cuerpo de solicitud
                          headers=data.headers)  # inserta los encabezados
-
-response = post_new_user(data.user_body)
-print(response.status_code)
-print(response.json())
 
 
 def post_new_client_kit(kit_body):
@@ -21,9 +16,3 @@ def post_new_client_kit(kit_body):
     return requests.post(configuration.URL_SERVICE + configuration.KITS_PATH,  # inserta la dirección URL completa
                          json=kit_body,  # inserta el cuerpo de solicitud
                          headers=headers)  # inserta los encabezados
-
-response = post_new_client_kit(data.kit_body)
-print(response.status_code)
-print(response.json())
-
-
